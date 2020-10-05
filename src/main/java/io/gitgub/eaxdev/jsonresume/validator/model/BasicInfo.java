6e03d6edbe34f0f6934886aab39296e7c0dd9cc0
eaxdev <eaxdev@ya.ru>
2020-10-05 21:26:49 +0300

    refactor: #5 cleanup

--- src/main/java/io/gitgub/eaxdev/jsonresume/validator/model/BasicInfo.java
+++ src/main/java/io/gitgub/eaxdev/jsonresume/validator/model/BasicInfo.java
@@ -1,6 +1,5 @@
 package io.gitgub.eaxdev.jsonresume.validator.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Getter;
